0cd626a9da836d599daa2745a300c124703018dd
Ross Kendall-Selwyn <Ross@Rosss-MBP.lan>
2020-06-07 20:40:29 +0100

    arrange code final headers and footers

--- php/index-contact.php
+++ php/index-contact.php
@@ -1,143 +1,119 @@
 <!DOCTYPE html>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="firstview-lettings.css">
-    <title>Contact Us</title>
+    <?= include('thunderbird.php'); ?>
+    <link rel="stylesheet" href="../firstview-lettings.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Contact Firstview Lettings</title>
   </head>
   <body>
-<div class="wrap">
-    <header class="main-header">
-        <a href="firstview-lettings-home.html"><img src="images/main-logo.jpg" id="main-logo"></a>
+      <div class="wrap">
+        <header style="display:flex">
+          <div><a href="../firstview-lettings-home.html"><img src="../images/main-logo.jpg" id="main-logo"></a></div>
             <nav class="main-nav">
-                  <li><a  class="dropbtn" href="firstview-lettings-home.html">Home</a></li>
-              <nav class="dropdown">
-                <ul>
-                  <li><a href="#">Landlords</a></li>
-                        <div class="dropdown-content">
-                          <li><a href="tenancy_application.html">Tenancy Applications</a></li>
-                          <li><a href="tenant-introduction-services.html">Tenant Introduction Service</a></li>
-                          <li><a href="inventory-preparation-services.html">Tenant Introduction & Inventory Preparation Service</a></li>
-                          <li><a href="letting-management.html">Letting Management Service</a></li>
-                        </div>
-                </ul>
-              </nav>
-              <nav class="dropdown">
-                <ul>
-                  <li><a href="#">Tenants</a></li>
-                        <div class="dropdown-content">
-                          <li><a href="tenancy_application.html">Tenancy Applications</a></li>
-                          <li><a href="tenant-enquiry.html">Tenant Enquiries</a></li>
-                        </div>
-                </ul>
-              </nav>
-              <nav class="dropdown">
-                <ul>
-                  <li><a href="#">About Us</a></li>
-                        <div class="dropdown-content">
-                          <li><a href="tenancy_application.html">Tenancy Applications</a></li>
-                          <li><a href="tenant-introduction-services.html">Tenant Introduction Service</a></li>
-                          <li><a href="inventory-preparation-services.html">Tenant Introduction & Inventory Preparation Service</a></li>
-                          <li><a href="letting-management.html">Letting Management Service</a></li>
-                        </div>
-                </ul>
-              </nav>
-              <nav class="dropdown">
-                <ul>
-                  <li><a href="#">Properties</a></li>
-                        <div class="dropdown-content">
-                          <li><a href="tenancy_application.html">Tenancy Applications</a></li>
-                          <li><a href="tenant-introduction-services.html">Tenant Introduction Service</a></li>
-                          <li><a href="inventory-preparation-services.html">Tenant Introduction & Inventory Preparation Service</a></li>
-                          <li><a href="letting-management.html">Letting Management Service</a></li>
-                        </div>
-                </ul>
-              </nav>
-              <nav class="dropdown">
-                <ul>
-                  <li><a href="#">Contact Us</a></li>
-                        <div class="dropdown-content">
-                          <li><a href="contact-us.html">Enquiry Form</a></li>
-                          <li><a href="#">Telephone Contact: 07834 342897</a></li>
-                          <li><a href="inventory-preparation-services.html">Tenant Introduction & Inventory Preparation Service</a></li>
-                          <li><a href="letting-management.html">Company Details:
-  Firstview Lettings Ltd.
-  Registration No: 5098005
+              <a href="../firstview-lettings-home.html">Home</a>
+              <a href="../properties.html">Properties</a>
+              <div class="drp">
+                <a href="../landlord-services.html">Landlords</a>
+                  <div class="ondrop">
+                    <li><a href="../tenancy_application.html">Tenancy Applications</a></li>
+                    <li><a href="../tenant-introduction-service.html">Tenant Introduction Service</a></li>
+                    <li><a href="../inventory-preparation-services.html">Tenant Introduction & Inventory Preparation Service</a></li>
+                    <li><a href="../letting-management.html">Letting Management Service</a></li>
+                  </div>
+              </div>
+              <div class="drp">
+              <a href="../tenant-services.html">Tenants</a>
+                <div class="ondrop">
+                  <li><a href="../tenancy_application.html">Tenancy Applications</a></li>
+                  <li><a href="../tenant-enquiries.html">Tenant Enquiries</a></li>
+                </div>
+              </div>
+              <a href="../about-firstview-lettings.html">About</a>
+              <a href="../php/index-contact.html">Contact</a>
+            </nav>
+        </header>
 
-  Registered Office:
-  367B Church Rd, Frampton Cotterell, Bristol, BS36 2AQ</a></li>
-                        </div>
-                </ul>
-              </nav>
-              </nav>
-    </header>
-    <main>
+          <main>
+            <h2 class="contact-title subp-title">How can we help today?</h2>
+            <div class="cstandard">
+              <container class="contact-flex">
+                <div class="contact-form">
+                  <h3 class="contact-title">Contact form</h3>
+                    <form class="contact-form" action="<?= $_SERVER['PHP_SELF']; ?>" method="post" id="form">
+                          <div class="success" value="<?= $success ?> "><?= $success ?></div>
+                          <p>
+                            <label for="name">Full Name:</label><br>
+                            <input type="text" id="name" name="name" placeholder="Please enter your name" value="<?= $name ?>" tabindex="0" >
+                            <span class="error"><?= $nameError ?></span>
+                          </p>
+                          <p>
+                            <label for="email">Email Address:</label><br>
+                            <input type="email" id="email" name="email" placeholder="Please enter your email address" value="<?= $address ?>" tabindex="1">
+                            <span class="error"><br><?= $mailFromError?></span>
+                          </p>
+                          <p>
+                            <label for="phone">Phone Number:</label><br>
+                            <input type="tel" id="phone" name="phone" placeholder="00000-000000" maxlength="13" minlength="11" value="<?= $phone ?>" tabindex="2"></input>
+                            <span class="error"><?= $phoneError?></span>
+                          </p>
+                          <p>
+                            <label for="address">Postcode:</label><br>
+                            <input type="text" name="address" placeholder="Please enter your postcode" value="<?= $address ?>" tabindex="4"></input><br>
+                            <span class="error"><?= $addressError?></span>
+                          </p>
+                          <p>
+                            <label for="subject">Subject</label><br>
+                            <input type="text" id="subject" name="subject" placeholder="Please enter a subject" value="<?= $messageSubject?>" tabindex="3" ></input>
+                            <span class="error"><?= $messageSubjectError?></span>
+                          </p>
+                          <p>
+                            <label for="body">Comments:</label><br>
+                            <textarea name="body" placeholder="Please enter your message" value="<?= $messageBody ?>" tabindex="5"></textarea>
+                            <span class="error"><?= $messageBodyError ?></span>
+                          </p>
+                          <p>
+                            <input type="radio" id="morning" name="time" value="morning" tabindex="6">Morning</input>
+                            <input type="radio" id="afternoon" name="time" value="afternoon">Afternoon</input>
+                            <input type="radio" id="anytime" name="time" value="anytime">Anytime</input>
+                          </p>
+                          <button type="submit" id="submit" name="submit" tabindex="7">Send Message</button>
+                          <span><a href="../privacy-statement.html">Privacy Policy</a></span>
+                    </form>
+                </div>
 
-            <h2>Contact Firstview Lettings Ltd</h2>
-            <p class="normal">Contact us at the details below, or complete the contact form and we'll be in touch shortly.</p>
-            <container class="contact-flex">
-            <div class="details">
-              <h2>Company Details</h2>
-              <p>Phone:<br> 07834 342897 </p>
-              <p>Email: firstview.let@btinternet.com</p>
-              <p>Registration No:<br> 5098005</p>
-              <p>Registered Office:<br>
-              367B Church Rd, Frampton Cotterell, Bristol, BS36 2AQ</p>
-          </div>
+                <div class="details cstandard">
+                  <h2>Company Details</h2>
+                  <p>Phone:<br> 07834 342897 </p>
+                  <p>Email:<br> firstview.let@btinternet.com</p>
+                  <p>Registration No:<br> 5098005</p>
+                  <p>Registered Office:<br>
+                  367B Church Rd, Frampton Cotterell, Bristol, BS36 2AQ</p>
+                </div>
+              </container>
+            </div>
+          </main>
+      </div>
 
-            <div class="contact-form">
-              <h2>Contact form</h2>
-            <form class="contact-form" action="contactform.php" method="post">
-              <fieldset>
-                <fieldset>
-                  <legend>Personal Details</legend>
-                  <p>
-                    <label for="name">First Name:</label>
-                    <input type="text" id="name" name="name"  placeholder="Please enter your name" required>
-                  </p>
-                </fieldset>
-            <fieldset>
-              <legend>Contact Details</legend>
-                <p>
-                  <label for="email">Email Address:</label>
-                  <input type="email" id="email" name="mail" required placeholder="Please enter your email address">
-                </p>
-                <p>
-                <label for="phone">Phone Number:</label>
-                  <input type="text" id="phone" name="phone" required="yes"></input>
-                </p>
-                <p>
-                <label for="address">Address:</label>
-                <textarea name="address" rows="5" cols="60"></textarea><br>
-                </p>
-            </fieldset>
-            <fieldset>
-              <legend>Message</legend>
-                <p>
-                <label for="comments">Comments:</label>
-                <textarea name="message" placeholder="enter your message"></textarea>
-              </p>
-            </fieldset>
-              <p>
-                <legend>Time to contact</legend>
-                  <input type="checkbox" id="morning" name="time">Morning</input>
-                  <input type="checkbox" id="afternoon" name="afternoon">Afternoon</input>
-              </p>
-              <button type="submit" id="submit" name="submit">Send Mail</button>
-              <span><a href="privacy-statement.html">Privacy Policy</a></span>
-            </fieldset>
+  <footer class="main-footer">
+    <div>
+      <span>Enquiries</span><br>
+      <a href="tel:07834 342897">07834 342897</a><br>
+      <a href="mailto:firstview.let@btinternet.com">firstview.let@btinternet.com</a>
+    </div>
+    <div class="resources">
+      <span>Resources</span><br>
+      <a>The Property Ombudsman</a>
+    </div>
+    <span id="copy">&copy Firstview Lettings Ltd 2004-2020</span>
+    <div>
+     <a href="legal.html" id="legal">Legal Notices</a><br>
+     <a href="../privacy-statement.html">Privacy Policy</a>
+    </div>
+    <span class="webdev">Crafted by Firstview Lettings Ltd</span>
+  </footer>
 
-            </form>
-          </div>
-
-
-          </div>
-  </main>
-  </container>
-      <footer class="main-footer">
-         <span><a href="legal.html" id="legal">Legal Notices</a></span>
-         <span id="copy">&copy Firstview Lettings Ltd 2004-2020.</span>
-      </footer>
-  <span class="webdev"><p>Website Developer Firstview Lettings Ltd</p></span>
   </body>
 </html>
